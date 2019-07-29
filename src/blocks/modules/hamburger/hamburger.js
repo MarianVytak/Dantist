82a3df2546610b3a015404add94e6867acc91e41
MarianVytak <marianvytak@gmail.com>
2019-07-29 12:50:45 +0300

    Intro: added mobile version

--- src/blocks/modules/hamburger/hamburger.js
+++ src/blocks/modules/hamburger/hamburger.js
@@ -5,9 +5,6 @@ import $ from 'jquery';
 // Hamburger
 const hamburger = $('.hamburger');
 const menu = $('.menu-mobile');
-// const menuLink = $('.menu__list_item_link');
-// const body = $('body');
-// const overlay = $('.overlay');
 
 hamburger.on('click', function(e){
 
@@ -15,32 +12,5 @@ hamburger.on('click', function(e){
 
     hamburger.toggleClass('hamburger-active');
     menu.slideToggle();
-    // body.toggleClass('hidden');
-    // overlay.toggleClass('active');
 
 });
-
-// menuLink.on('click', function (e) {
-//
-//     e.preventDefault();
-//
-//     hamburger.removeClass('hamburger-active');
-//     menu.removeClass('active');
-//     body.removeClass('hidden');
-//     overlay.removeClass('active');
-//
-//     let menuLinkScroll = $(this).attr('href'),
-//         menuLinkScrollBlock = ($(menuLinkScroll).offset().top) - 60;
-//     $('html, body').animate({
-//         scrollTop: menuLinkScrollBlock
-//     }, 2000);
-//
-// });
-//
-// body.on('click', '.overlay', function(){
-//     body.removeClass('active');
-//     overlay.removeClass('active');
-//     hamburger.removeClass('hamburger-active');
-//     menu.removeClass('active');
-//     body.removeClass('hidden');
-// });

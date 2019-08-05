62ec0e7392fcc270bc67fab0fcae95aa29eff759
MarianVytak <marianvytak@gmail.com>
2019-08-05 17:14:50 +0300

    Mobile menu: added scroll

--- src/blocks/modules/hamburger/hamburger.js
+++ src/blocks/modules/hamburger/hamburger.js
@@ -5,12 +5,16 @@ import $ from 'jquery';
 // Hamburger
 const hamburger = $('.hamburger');
 const menu = $('.navigation');
+const body = $('body');
+const header = $('.header');
 
 hamburger.on('click', function(e){
 
     e.preventDefault();
 
     hamburger.toggleClass('hamburger-active');
+    body.toggleClass('hidden');
+    header.toggleClass('active');
     menu.slideToggle();
 
 });

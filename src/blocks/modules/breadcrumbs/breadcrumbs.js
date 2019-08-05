d54a308a061de70e372782c948b77f378a33c233
MarianVytak <marianvytak@gmail.com>
2019-08-05 03:34:09 +0300

    Added anchor link

---
+++ src/blocks/modules/breadcrumbs/breadcrumbs.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+
+'use strict';
+
+const anchorLink = $('.breadcrumbs__list_item_link');
+
+anchorLink.on('click', function(e){
+    e.preventDefault();
+
+    var navScroll = $(this).attr('href'),
+        navScrollBlock = $(navScroll).offset().top;
+    $('html, body').animate({
+        scrollTop: navScrollBlock
+    }, 2000);
+});
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,4 +1,5 @@
 import "%modules%/hamburger/hamburger";
+import "%modules%/breadcrumbs/breadcrumbs";
 import "./_slick-customs";
 import "./_fancybox";
 import "./_counter";
